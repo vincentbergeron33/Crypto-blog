3248125b29251ae7067f449abf901d04a39149cf
Vincent Bergeron <vincentbergeron33@gmail.com>
2023-06-05 21:17:06 +0000

    add startboostrap kit and start view

--- blog/models.py
+++ blog/models.py
@@ -36,9 +36,11 @@ class Post(models.Model):
     def number_of_likes(self):
         return self.likes.count()
 
+
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
-    name= models.CharField(max_length=80)
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
     email = models.EmailField()
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
