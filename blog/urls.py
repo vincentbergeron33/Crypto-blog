23bbc52e539838a8b8a67037e5c667d9f65714ff
Vincent Bergeron <vincentbergeron33@gmail.com>
2023-06-09 15:32:30 +0000

    add view categorie and urls for categories

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('category/<str:category_name>/', views.CategoryPosts.as_view(), name='category_posts'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,50 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Post
 
 
-class PostList(generic.ListView):
-    model = Post
-    queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = 'index.html'
+class PostList(View):
+    def get(self, request, *args, **kwargs):
+        categories = ['Crypto News', 'Bitcoin', 'Exchange', 'Decentralize']
+        all_posts = []
+        for each_category in categories:
+            category_posts = Post.objects.filter(categorie=each_category)[:4]
+            all_posts.append({"category": each_category, "posts": category_posts})
+        
+        return render(request, "index.html", {
+          "all_posts": all_posts,
+        })
+
+    # model = Post
+    # queryset = Post.objects.filter(status=1).order_by('-created_on')
+    # template_name = 'index.html'
+
+
+class CategoryPosts(View):
+     def get(self, request, category_name, *args, **kwargs):
+        queryset = Post.objects.filter(categorie=category_name)
+        return render(
+            request,
+            "category_posts.html",
+            {
+                "posts": queryset,
+            },
+        )
+
+class PostDetail(View):
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "liked": liked
+            },
+        )
