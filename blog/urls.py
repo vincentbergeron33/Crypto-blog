934c6a9e3cb2d374871df5d480b92f043c40344e
Vincent Bergeron <vincentbergeron33@gmail.com>
2023-06-12 14:29:55 +0000

    add post scam, scam list and scam detail

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,14 @@
 from . import views
 from django.urls import path
+from blog.views import add_scam
 
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    path('scam/', views.ScamList.as_view(), name='scam'),
     path('report_scam/', views.PostScam.as_view(), name='report_scam'),
+    path('report_scam/add', add_scam, name='add'),
+    path('<slug:slug>/', views.ScamDetail.as_view(), name='scam_detail'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('category/<str:category_name>/', views.CategoryPosts.as_view(),
          name='category_posts'),
--- blog/views.py
+++ blog/views.py
@@ -18,9 +18,14 @@ class PostList(View):
           "all_posts": all_posts,
         })
 
-    # model = Post
-    # queryset = Post.objects.filter(status=1).order_by('-created_on')
-    # template_name = 'index.html'
+
+class ScamList(generic.ListView):
+    def get(self, request, *args, **kwargs):
+        model = Scam
+        queryset = Scam.objects.order_by('-created_on')
+        template_name = 'scam.html'
+
+        return render(request, 'scam.html')
 
 
 class CategoryPosts(View):
@@ -108,9 +113,68 @@ class PostScam(View):
         }
         return render(request, 'report_scam.html', context)
 
-    def post(request, *args, **kwargs):
-        scams = Scam.objects.all()
-        context = {
-            'scams': scams
-        }
-        return render(request, 'report_scam.html', context)
+
+def add_scam(request, *args, **kwargs):
+    if request.method == 'POST':
+        title = request.POST.get('scam_title')
+        media = request.POST.get('scam_media')
+        excerpt = request.POST.get('scam_excerpt')
+        content = request.POST.get('scam_content')
+        Scam.objects.create(
+            title=title, media=media, excerpt=excerpt, content=content)
+
+        return redirect('home')
+
+    return render(request, 'report_scam.html')
+
+
+class ScamDetail(View):
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Scam.objects.filter(status=1)
+        scam = get_object_or_404(queryset, slug=slug)
+        comments = scam.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+        return render(
+            request,
+            "scam_detail.html",
+            {
+                "scam": scam,
+                "comments": comments,
+                "commented": False,
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
+        )
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        scam = get_object_or_404(queryset, slug=slug)
+        comments = scam.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+        else:
+            comment_form = CommentForm()
+
+        return render(
+            request,
+            "scam_detail.html",
+            {
+                "scam": scam,
+                "comments": comments,
+                "commented": True,
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
+        )
