c6fdb5490e5a899e07b6af802fc492d094482510
Vincent Bergeron <vincentbergeron33@gmail.com>
2023-10-26 19:37:43 +0000

    Redeployment

--- cryptoblog/settings.py
+++ cryptoblog/settings.py
@@ -30,11 +30,11 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-vincentberge-cryptoblog-e7opt3c0v2i.ws-eu105.gitpod.io',
-    "https://cryptoblog-9d57f9a3fbcc.herokuapp.com/", "localhost"]
+    "cryptoblog-9d57f9a3fbcc.herokuapp.com", "localhost"]
 
 
 # Application definition
